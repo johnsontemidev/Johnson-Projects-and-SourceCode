3396b98c5e0b1b01808888e3c18063ee1ab22715
johnsontemidev <50930987+johnsontemidev@users.noreply.github.com>
2019-09-12 15:51:47 +0200

    Add files via upload
    Java Snake Game

---
+++ Java-Snake Game/Snakemain.java
@@ -0,0 +1,32 @@
+package johnsonJavaSnake;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snakemain extends JFrame {
+
+    public Snakemain() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snakemain();
+            ex.setVisible(true);
+        });
+    }
+}
